7b610fdcdb7e3a2329dcbd8c406dccd186f61eb4
Bruno <brunochelimo@gmail.com>
2023-02-07 11:39:56 +0300

    done

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -9,11 +9,29 @@ describe '#greet_programmer' do
   
 end
 
+# TEST 1
+def greet_programmer 
+  puts "Hello, programmer!"
+
+end
+
+
 describe '#greet' do
 
   it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
     expect { greet("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
   end
+
+
+# TEST 2
+
+  def greet(name) 
+      puts "Hello, #{name}!"
+
+  
+  end
+   
+
 
   it 'outputs a string "Hello, Jimmy!" when called with "Jimmy"' do
     expect { greet("Jimmy") }.to output(a_string_including("Hello, Jimmy!")).to_stdout
@@ -21,16 +39,40 @@ describe '#greet' do
 
 end
 
+# TEST3 
+
+def greet(name) 
+  puts "Hello, #{name}!"
+
+end
+
+
+
+
 describe '#greet_with_default' do
 
   it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
     expect { greet_with_default("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
   end
 
+  # TEST4
+
+  def reet_with_default (name = "Naureen") 
+      puts "Hello, #{name}!"
+    
+    end
+
+
   it 'outputs a string "Hello, programmer!" when called with no arguments' do
     expect { greet_with_default }.to output(a_string_including("Hello, programmer!")).to_stdout
   end
 
+end
+
+# TEST5
+
+def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
 end
 
 describe '#add' do
@@ -40,6 +82,16 @@ describe '#add' do
   end
 
 end
+
+  # TEST6
+ 
+  def add(num1, num2)
+    num1 + num2
+
+  end
+  
+
+
 
 describe '#halve' do
   
@@ -52,3 +104,11 @@ describe '#halve' do
   end
 
 end
+
+# TEST7
+
+def halve(num)
+  return nil unless num.is_a? Integer
+  num / 2
+end
+
